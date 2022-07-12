fb6d1535ef71e2aa8484c75742bf9ab3c709ff1f
niksync1 <niksync@gmail.com>
2022-07-12 21:21:09 +0000

    first commit with node express

---
+++ routes/promoRouter.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+    .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send all the promos to you');
+        })
+    .post((req,res,next) => {
+        res.end('Will add the promo: ' + req.body.name + ' with details: ' + req.body.description );
+    })
+    .put((req,res,next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /promos')
+    })
+    .delete((req,res,next) => {
+        res.end('Deleting all promos!');
+    });
+
+promoRouter.route('/:dishId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+    })
+.get((req, res, next) => {
+        res.end('Will send details of the promo: ' 
+        + req.params.dishId + ' to you!');
+    })
+    
+.post((req,res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on promos endpoint');
+    })
+    
+.put((req,res, next) => {
+        res.write('Updating the promo: ' + req.params.dishId + '. ')
+        res.end('Will update the promo: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    
+.delete((req,res, next) => {
+        res.statusCode = 403;
+        res.end('deleting the promo: ' + req.params.dishId);
+    });
+module.exports = promoRouter;
